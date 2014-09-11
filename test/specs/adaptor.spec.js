dfd1f55b0fb97b09a212b6049cd1ae220394e5be
deadprogram <ron@hybridgroup.com>
2014-09-10 18:56:17 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Hue = source("adaptor");
+
+describe("Cylon.Adaptors.Hue", function() {
+  var adaptor = new Hue();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Hue = source("driver");
+
+describe("Cylon.Drivers.Hue", function() {
+  var driver = new Hue({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
