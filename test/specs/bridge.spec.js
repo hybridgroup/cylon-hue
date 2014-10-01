f964215d313212571e5d3d3a2602fe3228a4d8e4
deadprogram <ron@hybridgroup.com>
2014-10-01 07:15:47 -0700

    Add stub for Bridge tests

---
+++ test/specs/bridge.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Bridge = source("light");
+
+describe("Cylon.Drivers.Hue.Bridge", function() {
+  var driver = new Bridge({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
